877a66adf3feb3f37ba52b559ca3fbe7aca93589
amit05sept <gladiatorpandey@gmail.com>
2024-03-20 21:48:27 +0530

    week 6 assignments done

--- week-6/3-use-ref/src/App.jsx
+++ week-6/3-use-ref/src/App.jsx
@@ -5,10 +5,10 @@ import { Assignment2 } from './components/Assignment2'
 function App() {
   return (
     <>
-      <Assignment1 />
-      {/* <Assignment2 /> */}
+      {/* <Assignment1 /> */}
+      <Assignment2 />
     </>
   )
-}
+} 
 
 export default App
